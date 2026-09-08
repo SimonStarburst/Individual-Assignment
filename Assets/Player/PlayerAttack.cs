9338304c6bbf2fe84a1f3e3ac08b56117233fa0c
SimonStarburst <326016527+SimonStarburst@users.noreply.github.com>
2026-09-08 19:31:08 +0200

    Player stats and enemy distinction
    Worked more on making scripts work with their corresponding GameObjects and added some slight distinctions to enemy types. Started to question whether I wanted to continue with this 2D project or make it in 3D instead, created 3D project as well to try things out.

--- Assets/Player/PlayerAttack.cs
+++ Assets/Player/PlayerAttack.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
+
 {
+
+    public GameObject playerAttack;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Player/PlayerStats.cs
+++ Assets/Player/PlayerStats.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
+
 {
+
+    public PlayerMovement movementStats;
+    public PlayerAttack attackStats;
+
+
+    private void Awake()
+    {
+        // I will be able to adjust variables in the movement and attack scripts, for level up purposes. 
+        movementStats = GetComponent<PlayerMovement>();
+        attackStats = GetComponent<PlayerAttack>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +26,7 @@ public class PlayerStats : MonoBehaviour
     {
         
     }
+
+
+
 }
