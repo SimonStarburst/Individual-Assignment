43bef505d9c5309dc324891fdb6f149e577640f4
SimonStarburst <326016527+SimonStarburst@users.noreply.github.com>
2026-09-25 11:56:46 +0200

    Added more movement to Close ranged enemies
    Now both Enemy Close Small and Big follows the player, at different speeds. Next job is to create scriptable objects for enemy stats

--- Assets/Scripts/Enemy/Close Range/CloseMovement.cs
+++ Assets/Scripts/Enemy/Close Range/CloseMovement.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class CloseMovement : MonoBehaviour
 {
+    #region Variables
+
     public Transform player;
     public float smallMoveSpeed;
     public float empoweredSmallMoveSpeed;
     public float bigMoveSpeed;
     public float empoweredBigMoveSpeed;
     public float bossMoveSpeed;
+    #endregion
 
     private void Awake()
     {
@@ -21,12 +24,36 @@ public class CloseMovement : MonoBehaviour
 
     private void Update()
     {
-        EnemyCloseMovement();
+        SmallMovement();
+        BigMovement();
+        SmallEmpoweredMovement();
+        BigEmpoweredMovement();
+        BossMovement();
     }
 
-    private void EnemyCloseMovement()
+    private void SmallMovement()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, (smallMoveSpeed * Time.deltaTime));
+    }
+
+    private void BigMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, (bigMoveSpeed * Time.deltaTime));
+    }
+
+    private void SmallEmpoweredMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, (empoweredSmallMoveSpeed * Time.deltaTime));
+    }
+
+    private void BigEmpoweredMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, (empoweredBigMoveSpeed * Time.deltaTime));
+    }
+
+    private void BossMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, (bossMoveSpeed * Time.deltaTime));
     }
 
 
